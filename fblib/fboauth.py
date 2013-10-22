2afddd39d0f1ee3970c38eb8bd4e11fe1faee449
Kiran Panesar <Kiran@MobileXLabs.com>
2013-10-22 18:28:50 +0100

    Updated fblib (Addresses #14)

---
+++ fblib/fboauth.py
@@ -0,0 +1,79 @@
+# #!/usr/bin/env python
+# # encoding: utf-8
+#
+# Created by Kiran Panesar - 22/10/2013
+
+import webbrowser
+import csv
+import time
+
+from Tkinter import *
+
+class FBOAuthManager(object):
+	"""docstring for FBOAuthManager"""
+	def __init__(self, client_id, client_secret, redirect_uri):
+		super(FBOAuthManager, self).__init__()
+		self.access_token = None
+		self.access_token_expirery_epoch = 0
+		
+		self.client_id     = client_id
+		self.client_secret = client_secret
+		self.redirect_uri  = redirect_uri
+		
+		self.load_access_token()
+
+	def fb_authenticate_user(self, client_id=None, redirect_uri=None, client_secret=None):
+		if client_id == None:
+			client_id = self.client_id
+		if client_secret == None:
+			client_secret = self.client_secret
+		if redirect_uri == None:
+			redirect_uri = self.redirect_uri
+
+		urlString = "https://www.facebook.com/dialog/oauth?client_id="+client_id+"&redirect_uri="+redirect_uri+"&response_type=token"
+		webbrowser.get('windows-default').open(urlString)
+
+		master = Tk()
+		master.title("Sign Into Facebook")
+
+		e = Entry(master, width=50)
+		e.pack()
+
+		e.focus_set()
+		e.insert(0,"")
+
+		def callback_block():
+		    if len(e.get()) > 0:
+		    	# parse the url
+		    	url_string = e.get()
+		    	self.access_token = url_string[url_string.find("=")+1:url_string.find("&expires_in")]
+		    	expiration_epoch = url_string[url_string.find("&expires_in=")+12:]
+		    	print expiration_epoch
+		    	self.save_access_token(self.access_token, expiration_epoch)
+
+		    master.destroy()
+
+		b = Button(master, text="Sign In!", width=10, command=callback_block)
+		b.pack()
+
+		mainloop()
+	def load_and_check_access_token(self):
+		if self.access_token_expirery_epoch <= time.time():
+			self.fb_authenticate_user()
+		else:
+			self.load_access_token()
+
+	def load_access_token(self):
+		with open('./fblib/token_data.csv', 'rb') as csvfile:
+			csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+			for row in csv_reader:
+				self.access_token = row[0]
+				self.access_token_expirery_epoch = row[1]
+
+	def save_access_token(self, access_token, expirery_epoch):
+		with open("./fblib/token_data.csv", "w+") as csv_file:
+			csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			expiration_epoch = int(expirery_epoch) + int(time.time())
+
+			csv_writer.writerow([access_token, expiration_epoch])
+		self.load_access_token
--- fblib/fbrequest.py
+++ fblib/fbrequest.py
@@ -4,15 +4,30 @@
 # Created by Kiran Panesar - 22/10/2013
 
 import urllib
+import fboauth
 
 class FBRequestManager(object):
 	"""docstring for FBRequestManager"""
-	def __init__(self, access_token):
+	def __init__(self, client_id, client_secret, redirect_uri):
 		super(FBRequestManager, self).__init__()
-		self.access_token = access_token
+		self.client_id     = client_id
+		self.client_secret = client_secret
+		self.redirect_uri  = redirect_uri
+
+		self.fb_oauth_manager = fboauth.FBOAuthManager(self.client_id, self.client_secret, self.redirect_uri)
+
+	# Used to sign the user in 
+	def fb_authenticate_user(self):
+		self.fb_oauth_manager.fb_authenticate_user(self.client_id, self.redirect_uri, self.client_secret)
+
+	# Loads info on the currently-auth'd user
+	def fb_load_info_for_current_user(self):
+		self.fb_oauth_manager.load_and_check_access_token()
+		return urllib.urlopen("https://graph.facebook.com/me?access_token="+self.fb_oauth_manager.access_token).read()
+
+	# Posts a status as the currently-auth'd
 	def fb_post_message(self, status_message):
-		data = {"access_token" : self.access_token, "message" : status_message}
+		self.fb_oauth_manager.load_and_check_access_token()
+		data = {"access_token" : self.fb_oauth_manager.access_token, "message" : status_message}
 		data = urllib.urlencode(data)
-		print data
-		response = urllib.urlopen("https://graph.facebook.com/1071692196/feed", data)
-		print response.info()
+		urllib.urlopen("https://graph.facebook.com/me/feed", data)
