aff1018d0b79ec983ca4a9d33c2b65d58d271fdd
dandanarno <dandanarno@gmail.com>
2013-10-23 17:20:09 +0100

    Boat functionality, working
    fixes #10

--- engine.py
+++ engine.py
@@ -19,7 +19,6 @@ class GameEngine(object):
 				self.right_shore_characters = []
 				self.sound = True
 
-
 		def draw_background(self):
 				blue = 52, 152, 219
 				self.screen.fill(blue)
@@ -41,6 +40,8 @@ class GameEngine(object):
 				self.player = character.Character(gifsprite.Create(['Farmer01.png', 'Farmer02.png','Farmer03.png','Farmer04.png'], 75, 128))
 				self.player.game_image.rect.top = 265
 				self.player.game_image.rect.left = 10
+				self.player.direction = 0
+				self.player.step_interval = 50
 
 				self.cabbage = character.Character(gifsprite.Create(['Cabbage01.png', 'Cabbage02.png','Cabbage03.png','Cabbage04.png'], 123, 100))
 				self.cabbage.game_image.rect.top = 290
@@ -49,25 +50,23 @@ class GameEngine(object):
 				self.cabbage.positionRight = 850
 
 				self.wolf = character.Character(gifsprite.Create(['Wolf01.png', 'Wolf02.png','Wolf03.png'], 64, 40))
-				self.wolf.game_image.rect.top = 340
-				self.wolf.game_image.rect.left = 180
+				self.wolf.game_image.rect.top = 350
+				self.wolf.game_image.rect.left = 200
 
-				self.wolf.positionLeft = 110
+				self.wolf.positionLeft = 200
 				self.wolf.positionRight = 790
 
 				self.sheep = character.Character(gifsprite.Create(['Sheep01.png', 'Sheep02.png'], 40, 40))
-				self.sheep.game_image.rect.top = 320
-				self.sheep.game_image.rect.left = 180
+				self.sheep.game_image.rect.top = 350
+				self.sheep.game_image.rect.left = 170
 
 				self.sheep.positionLeft = 170
 				self.sheep.positionRight = 730
 
 				self.boat = gifsprite.Create(['Boat-02a.png', 'Boat-03a.png'], 163, 90)
-				self.boat.rect.top = 290
-				self.boat.rect.left = 220
-
-				self.boat.positionLeft = 370
-				self.boat.positionRight = 730
+				self.boat.rect.top = 325
+				self.boat.rect.centerx = 325
+				self.boat.binding = 0
 
 				self.left_shore_characters.append(self.cabbage)
 				self.left_shore_characters.append(self.wolf)
@@ -75,18 +74,34 @@ class GameEngine(object):
 
 				self.refresh_characters()
 
+		def boat_binding(self):
+			# only called when player on water
+			if self.player.direction: #going right
+				self.boat.setFlip(0)
+				if self.player.game_image.rect.centerx > self.boat.rect.centerx:
+					self.boat.binding = self.player.game_image.rect.centerx - self.boat.rect.centerx
+			else: #Going left
+				self.boat.setFlip(1)
+				if self.player.game_image.rect.centerx < self.boat.rect.centerx:
+					self.boat.binding = self.player.game_image.rect.centerx - self.boat.rect.centerx
+
 		def refresh_characters(self):
 				self.draw_background()
 				
 				if self.player.equipped_item:
-					self.player.equipped_item.game_image.rect.top  = self.player.game_image.rect.top-self.player.equipped_item.game_image.rect.height
-					self.player.equipped_item.game_image.rect.left = self.player.game_image.rect.left
+					if self.player.direction:
+						# self.player.equipped_item.game_image.rect.left  = self.player.game_image.rect.top-self.player.equipped_item.game_image.rect.height
+						self.player.equipped_item.game_image.rect.right = self.player.game_image.rect.left
+					else:
+						self.player.equipped_item.game_image.rect.left = self.player.game_image.rect.right
 
 				self.player.game_image.group.draw(self.screen)
-				self.boat.group.draw(self.screen)
+				
 				self.cabbage.game_image.group.draw(self.screen)
 				self.wolf.game_image.group.draw(self.screen)
+
 				self.sheep.game_image.group.draw(self.screen)
+				self.boat.group.draw(self.screen)# Draw Last!
 
 		def deposit_item(self):
 				if self.player.equipped_item:
@@ -122,8 +137,11 @@ class GameEngine(object):
 		def check_characters(self):
 				current_side = []
 
+				if self.player.game_image.rect.right > 380 and self.player.game_image.rect.left < 620: # Keep, change values later for boat docking
+					self.boat_binding();
+
 				if self.player.game_image.rect.right > 380 and self.player.game_image.rect.left < 620:
-				# Check if the sheep has been left with the cabbage, or the wolf with the sheep
+					# Check if the sheep has been left with the cabbage, or the wolf with the sheep
 					if len(self.right_shore_characters) > 1:
 						if self.cabbage in self.right_shore_characters and self.sheep in self.right_shore_characters:
 							print "cabbage eaten by sheep"
@@ -208,28 +226,49 @@ class GameEngine(object):
 					for event in pygame.event.get():
 						if event.type == pygame.QUIT:
 							sys.exit()
-						elif event.type == pygame.KEYDOWN: # Check for keydown events       
+						elif event.type == pygame.KEYDOWN: # Check for keydown events   
+							self.boat.binding = 0    
 							if event.key == pygame.K_RIGHT: # Moving right
+								self.player.direction = 1
 								self.player.game_image.update()
 								self.player.game_image.setFlip(1)
 								if self.player.game_image.rect.right <= 850:
-									self.player.game_image.rect = self.player.game_image.rect.move([50, 0])
+									self.player.game_image.rect = self.player.game_image.rect.move([self.player.step_interval, 0])
 									self.check_characters()
 								elif self.player.game_image.rect.right != 900:
 									self.player.game_image.rect = self.player.game_image.rect.move([900 - self.player.game_image.rect.right, 0])
 									self.check_characters()
 							elif event.key == pygame.K_LEFT: # Moving left
+								self.player.direction = 0
 								self.player.game_image.update()
 								self.player.game_image.setFlip(0)
-								if self.player.game_image.rect.left >= 50:
-									self.player.game_image.rect = self.player.game_image.rect.move([-50,0])
+								if self.player.game_image.rect.left >= self.player.step_interval:
+									self.player.game_image.rect = self.player.game_image.rect.move([-(self.player.step_interval),0])
 									self.check_characters()
 								elif self.player.game_image.rect.left != 0:
 									self.player.game_image.rect = self.player.game_image.rect.move([0 - self.player.game_image.rect.left, 0])
 									self.check_characters()
 							elif event.key == pygame.K_ESCAPE: # Quit to title
 								self.title_screen();
+							
+							# Move boat
+							if self.boat.binding > 0:
+								position = self.boat.rect.centerx + self.boat.binding
+								if position	> 597:
+									self.boat.rect.centerx = 597
+								else: 
+									self.boat.rect.centerx = position
+								self.player.game_image.setImage(0)
+							elif self.boat.binding < 0:
+
+								position = self.boat.rect.centerx + self.boat.binding
+								if position	< 350:
+									self.boat.rect.centerx = 325
+								else: 
+									self.boat.rect.centerx = position
+								self.player.game_image.setImage(0)
 							self.refresh_characters()
+								
 						elif event.type == pygame.MOUSEBUTTONUP: # Mouse event
 							if self.player.equipped_item:     
 								self.deposit_item()
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import engine
 
 engine = engine.GameEngine()
 # engine.title_screen()
-engine.start_game()
+engine.title_screen()
 # import sys  
 # import pygame
 # import character
