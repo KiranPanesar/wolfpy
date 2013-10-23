60fdf4063e2d1f4fd22d87ca7e51be6b0acc9c98
Kiran Panesar <Kiran@MobileXLabs.com>
2013-10-23 15:49:37 +0100

    Added game over screen

--- game_over.py
+++ game_over.py
@@ -1,16 +1,22 @@
 import sys, pygame
 import lib.button as button
+import engine
 
 class GameOverScreen(object):
 	"""docstring for GameOverScreen"""
-	def __init__(self, screen):
+	# if cabbage kills sheep, scenario = 0
+	# if wolf kills sheep, scenario = 1
+	def __init__(self, screen, scenario):
 		super(GameOverScreen, self).__init__()
+		self.screen = screen
 
+		image = pygame.image.load("./img/Game Over Wolf Sheep.png").convert_alpha()
+		
 	def show_screen(self):
 		self.screen.fill((255, 255, 255))
-		
-		retry_button	  	=  button.Create(self.screen,(0,210,255), None, 180, "Try again", 300, 50, (255,255,255), 32, 2,(10,10,10),0)
-		main_menu_button 	=  button.Create(self.screen,(0,210,255), None, 180, "Main Menu", 300, 50, (255,255,255), 32, 2,(10,10,10),0)
+
+		retry_button	  	=  button.Create(self.screen,(0,210,255), None, 300, "Try again", 300, 50, (255,255,255), 32, 2,(10,10,10),0)
+		main_menu_button 	=  button.Create(self.screen,(0,210,255), None, 370, "Main Menu", 300, 50, (255,255,255), 32, 2,(10,10,10),0)
 
 		self.screen.blit(retry_button.render,retry_button.rect)
 		self.screen.blit(main_menu_button.render,main_menu_button.rect)
@@ -21,8 +27,13 @@ class GameOverScreen(object):
 						sys.exit()
 				elif event.type == pygame.MOUSEBUTTONUP:
 					pos = pygame.mouse.get_pos()
-
+					if main_menu_button.rect.collidepoint(pos):
+						print "Main menu clicked"
+						game_engine = engine.GameEngine()
+						game_engine.title_screen()
+					elif retry_button.rect.collidepoint(pos):
+						print "Rety button clicked"
+						game_engine = engine.GameEngine()
+						game_engine.start_game()
 
 			pygame.display.flip()
-
-		
