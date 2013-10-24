4d530fdf4d1f474e1fe083074dabbb8511563282
Kiran Panesar <Kiran@MobileXLabs.com>
2013-10-24 12:00:43 +0100

    fblib commented

--- fblib/fboauth.py
+++ fblib/fboauth.py
@@ -11,19 +11,31 @@ import os
 from Tkinter import *
 
 class FBOAuthManager(object):
-	"""docstring for FBOAuthManager"""
+	""" Class to handle the requesting, storing and checking of the Facebook access token
+		(Used to authenticate the current user)
+	"""
 	def __init__(self, client_id, client_secret, redirect_uri):
 		super(FBOAuthManager, self).__init__()
+
+		# Initial the current access token and expirery time
 		self.access_token = None
 		self.access_token_expirery_epoch = 0
 		
+		# Set up the data needed for authentication
 		self.client_id     = client_id
 		self.client_secret = client_secret
 		self.redirect_uri  = redirect_uri
 		
+		# Load any saved access token from the data store
 		self.load_access_token()
 
+	# Instance method to sign the user in using OAuth
+	# Parameters are optional, will default to current instance's properties if params are None
+	#	1) Web browser launches with authentication URL for the app
+	# 	2) User signs in, user pastes access token URL into pop up
+	# 	3) Application parses out access token
 	def fb_authenticate_user(self, client_id=None, redirect_uri=None, client_secret=None):
+		# Default to properties if the parameters aren't provided
 		if client_id == None:
 			client_id = self.client_id
 		if client_secret == None:
@@ -31,17 +43,22 @@ class FBOAuthManager(object):
 		if redirect_uri == None:
 			redirect_uri = self.redirect_uri
 
+		# Create the URL string to request the user to sign in
+		# Docs for this found at http://developers.facebook.com/docs/facebook-login/
 		url_string = "https://www.facebook.com/dialog/oauth?client_id="+client_id+"&redirect_uri="+redirect_uri+"&response_type=token&scope=publish_actions"
 
+		# Create Tkinter object to show pop up
 		master = Tk()
 
+		# Create callback function for if the user confirms they want to sign in
 		def confirm_log_in():
 			master.destroy()
 			log_in()
-			
+		
+		# Run the logic to log in
 		def log_in():
 			# University computers only allow apps to open in IE.
-			# So check if IE exists and open in that, if it doesn't, open in default.
+			# So check if IE exists and open request string in that, if it doesn't, open in default.
 			if os.path.isfile('c:\program files\internet explorer\iexplore.exe'):
 				print "IE"
 				browser = webbrowser.get('c:\program files\internet explorer\iexplore.exe')
@@ -50,38 +67,52 @@ class FBOAuthManager(object):
 				print "custom"
 				webbrowser.open(url_string)
 
+			# Create pop up box to enter the resulting login URL (used to parse out the access token)
 			master = Tk()
 			master.title("Sign Into Facebook")
 
+			# Add text box to pop up
 			e = Entry(master, width=50)
 			e.pack()
 
 			e.focus_set()
 			e.insert(0,"")
 
+			# Callback block to parse out the access_token from the URL
 			def callback_block():
+				# If text was entered
 			    if len(e.get()) > 0:
-			    	# parse the url
+			    	# Parse out the access token from the URL
+			    	# This needs to be perfected a bit
 			    	url_string = e.get()
 			    	self.access_token = url_string[url_string.find("=")+1:url_string.find("&expires_in")]
 			    	expiration_epoch = url_string[url_string.find("&expires_in=")+12:]
+
+			    	# Save the access token and expirery data to disk
 			    	self.save_access_token(self.access_token, expiration_epoch)
 
+			    # Close pop up
 			    master.destroy()
 
+			# Add sign in button to pop up
 			b = Button(master, text="Sign In!", width=10, command=callback_block)
 			b.pack()
-
+			# Show pop up
 			mainloop()
 
+		# Show pop up giving steps to sign in
 		master.title("Steps to Sign In")
 		w = Label(master, text="1) Log in using the browser window which will appear.\n2)Copy the URL that is loaded\n3)Paste back into the game\nAnd you're done!")
 		w.pack()
+
+		# Create button to callback to the confirm_log_in() function (defined line 54)
 		b = Button(master, text="Got it!", width=10, command=confirm_log_in)
 		b.pack()
 		mainloop()
 
+	# Instance method to sign the user out
 	def destroy_session(self):
+		# Reset the properties
 		self.access_token = None
 		self.access_token_expirery_epoch = 0
 
@@ -90,32 +121,44 @@ class FBOAuthManager(object):
 		csv_file = open(csv_file_name, "w+")
 		csv_file.close()
 
+	# Loads the access token (if available). If not, authenticate the user.
 	def load_and_check_access_token(self):
 		if self.has_access_token_expired():
 			self.fb_authenticate_user()
 		else:
 			self.load_access_token()
-	
+
+	# Check if the access token has expired
 	def has_access_token_expired(self):
+		# Expirery epoch (UNIX time) checked against current time
 		return self.access_token_expirery_epoch <= time.time()
 
+	# Check if the user is logged in
 	def is_user_logged_in(self):
+		# If the access token doesn't exist, return False
 		if self.access_token == None:
 			return False
 		else:
 			return True
 
+	# Loads the access token from the disk
 	def load_access_token(self):
+		# Creates path to CSV file
 		with open('./fblib/token_data.csv', 'rb') as csvfile:
 			csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+			# Grab the access token and expirery epoch
 			for row in csv_reader:
 				self.access_token = row[0]
 				self.access_token_expirery_epoch = row[1]
 
+	# Save the access token to the disk
 	def save_access_token(self, access_token, expirery_epoch):
+		# Find/create the CSV to store
 		with open("./fblib/token_data.csv", "w+") as csv_file:
 			csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 			expiration_epoch = int(expirery_epoch) + int(time.time())
-
+			# Store the access token
 			csv_writer.writerow([access_token, expiration_epoch])
+		
+		# Load token to ensure success
 		self.load_access_token
--- fblib/fbrequest.py
+++ fblib/fbrequest.py
@@ -8,55 +8,86 @@ import fboauth
 import json
 
 class FBError(object):
-	"""docstring for FBError"""
+	"""Class to return an error object if an FBRequestManager method fails"""
+
 	def __init__(self, error_message, error_code):
 		super(FBError, self).__init__()
 		self.error_code = error_code
 		self.error_message = error_message		
 
 class FBRequestManager(object):
-	"""docstring for FBRequestManager"""
+	""" Class to handle all requests to the Facebook API
+		The calling controller should check whether the response is of type FBError
+		to determine the success of the request.
+
+		All methods are prefixed with "fb_" 
+	"""
 	def __init__(self, client_id, client_secret, redirect_uri):
 		super(FBRequestManager, self).__init__()
+
+		# Initialise the application with the data needed to authenticate the user
 		self.client_id     = client_id
 		self.client_secret = client_secret
 		self.redirect_uri  = redirect_uri
 
+		# Set up our OAuth manager with the authentication data
+		# This class can be found in /fboatuh.py
 		self.fb_oauth_manager = fboauth.FBOAuthManager(self.client_id, self.client_secret, self.redirect_uri)
 
-	# Used to sign the user in 
+	# Instance method to sign the user in
 	def fb_authenticate_user(self):
+		# Runs OAuth manager method to sign in
 		self.fb_oauth_manager.fb_authenticate_user(self.client_id, self.redirect_uri, self.client_secret)
 
+	# Sign the user out
 	def fb_log_out(self):
 		self.fb_oauth_manager.destroy_session()
 		
 	# Loads info on the currently-auth'd user
 	def fb_load_info_for_current_user(self):
+		# Load the saved access token and checks that it's still valid
+		# If the user isn't auth'd, it will go and authenticate them 
 		self.fb_oauth_manager.load_and_check_access_token()
+
+		# Request the information for the user
 		response = urllib.urlopen("https://graph.facebook.com/me?access_token="+self.fb_oauth_manager.access_token).read()
 		
+		# Parse out the error from the JSON response
 		if error_from_json(json.loads(response)):
-			return error_from_json(json.loads(response))
+			return error_from_json(json.loads(response)) # If an error occurred, return the FBError object
 
+		# Return the parsed JSON
+		# Ideally we will implement an FBUser object to store the parsed JSON
 		return json.loads(response)
 
 	# Posts a status as the currently-auth'd
 	def fb_post_message(self, status_message):
+		# Load access token or authenticate user
 		self.fb_oauth_manager.load_and_check_access_token()
+
+		# Create the data to POST to the Facebook API
 		data = {"access_token" : self.fb_oauth_manager.access_token, "message" : status_message}
+		# Encode the POST data
 		data = urllib.urlencode(data)
+
+		# Make request to Facebook API
 		response = urllib.urlopen("https://graph.facebook.com/me/feed", data).read()
 
+		# Check for and return error. Will return None if request was successful
 		return error_from_json(json.loads(response))
 
+	# Simple method to check if the user is logged in
 	def fb_is_user_authd(self):
+		# Call FBOAuthManager method to check
 		return self.fb_oauth_manager.is_user_logged_in()
 
-
+# Parses the error object from the FB JSON response
 def error_from_json(json_dict):
 	json_error = None
+
 	if json_dict != None:
+		# If the JSON response contains an "error" object, return the FBError instance
+		# with the message and code
 		if "error" in json_dict:
 			json_error = FBError(json_dict["error"]["message"], str(json_dict["error"]["code"]))
 
