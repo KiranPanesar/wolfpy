c00fabbb08cce1270813ad93037089818860c2ee
Daniel Koehler <dandanarno@gmail.com>
2014-01-23 17:39:20 +0000

    Packaged commit. (Mac .App)
    No Facebook Intergration, If I rember I cut down a lot more also.

---
+++ River Crossing Adventure! (Cut Down Version).app/Contents/Resources/game.py
@@ -0,0 +1,10 @@
+# #!/usr/bin/env python
+# # encoding: utf-8
+#
+# Created by Kiran Panesar and Dan Koehler - 22/10/2013
+
+import engine
+
+# Load the engine instance and show it
+engine = engine.GameEngine(None)
+engine.title_screen()
